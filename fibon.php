171a6bc519b7678047fb0b56fdce5ffa8c973abf
Ivan Shim <zeusmicro@gmail.com>
2021-07-20 01:07:29 +0800

    Create fibon.php

---
+++ fibon.php
@@ -0,0 +1,29 @@
+<?php
+
+// fibonacci numbers and golden ratio
+// Ivan's bored doodlings
+// Ivan Shim 2021 July 20th
+
+echo 'fibonacci sequence: '."\n";
+
+$i = 1;
+$j = 0;
+$k = 1;
+while ($i<=40) {
+  echo $i;
+  $i++;
+  echo ', ';
+  $l = $k + $j;
+  $j = $k;
+  $k = $l;
+  echo $j;
+  echo ', ';
+  echo $k/$j;
+  echo "\n";
+}
+
+echo "\n";
+echo 'golden ratio: '.((1+sqrt(5))/2)."\n";
+echo 'the ratio of 2 consecutive Fibonacci numbers tends to the golden ratio as n incrases'."\n";
+
+?>
